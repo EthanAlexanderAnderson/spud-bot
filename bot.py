a48c981cb3f89ab388288aef6517eae53ed22b9d
Ethan Anderson <et714492@dal.ca>
2022-09-23 17:05:02 -0300

    combined some lines of dreamdebug to decrease message cooldown issues

--- bot.py
+++ bot.py
@@ -288,15 +288,12 @@ async def on_message(message):                                                  
 
     #debug
     elif message.content.startswith('/dreamdebug'):
-        await message.channel.send("Buffer Length: " + str(len(buffer)))
-        await message.channel.send("Buffer Content: " + (', ').join(map(str, buffer)))
-        await message.channel.send("Guesses: " + str(guesses))
-        await message.channel.send("Guessed: " + str(guessed))
+        await message.channel.send("Buffer Length: " + str(len(buffer)) + "\n" + "Buffer Content: " + (', ').join(map(str, buffer)))
+        await message.channel.send("Guesses: " + str(guesses) + "\n" + "Guessed: " + str(guessed))
         await message.channel.send("Scores: ")
         for player, score in scores.items():
             await message.channel.send("<@{}>: {}".format(player, score))
-        await message.channel.send("Players: " + str(players))
-        await message.channel.send("Channel Playing: " + str(channelplaying))
+        await message.channel.send("Players: " + str(players) + "\n" + "Channel Playing: " + str(channelplaying))
 
 
     # for scoring (must be at the bottom to not interfere with other commands)
