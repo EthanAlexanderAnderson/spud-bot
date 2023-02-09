1b7ffd03b8d3f4b96607e3872d482cf8d1ce7465
Ethan Anderson <et714492@dal.ca>
2023-02-09 00:04:15 -0400

    profile updates

--- bot.py
+++ bot.py
@@ -418,22 +418,29 @@ async def on_message(message):                                                  
         msg = message.content.split(" ")                                   # split message
         if len(msg) <= 2:
             # profile displaying
-            # note to self: profile stats should be under first name instead of ID, to support alt accounts
+            # TODO profile stats should be under first name instead of ID, to support alt accounts
             # stats:  dreamcount, all time score (exp), correct ratio, longest streak
             if len(msg) == 1:
                 # show profile of user who sent message
                 userID = message.author.id
                 name = redis.get("&" + str(userID))
                 await message.channel.send("Profile <@" + str(userID) + "> is assigned to " + name)
-                await message.channel.send("Total Corrects: " + redis.get("%" + name))
+                stats = redis.get("%" + name).split(",")
+                await message.channel.send("Total Corrects: " + stats[0])
+                await message.channel.send("Total Incorrects: " + stats[1])
                 await message.channel.send("More profile stats coming soon")
 
             elif len(msg) == 2:
                 # if 1 in names strict, show profile of that user
-                userID = msg[1][2:-1]
+                if msg[1][0] == "<":
+                    userID = msg[1][2:-1]
+                else:
+                    await message.channel.send("Please use the @ of the profile you wish to view.")
                 name = redis.get("&" + str(userID))
                 await message.channel.send("Profile <@" + str(userID) + "> is assigned to " + name)
-                await message.channel.send("Total Corrects: " + redis.get("%" + name))
+                stats = redis.get("%" + name).split(",")
+                await message.channel.send("Total Corrects: " + stats[0])
+                await message.channel.send("Total Incorrects: " + stats[1])
                 await message.channel.send("More profile stats coming soon")
 
         elif len(msg) > 2 and (msg[1].lower() == 'link' or msg[1].lower() == 'add') and msg[2].capitalize() in namesStrict:
@@ -488,7 +495,7 @@ async def on_message(message):                                                  
             scores[playerID] += 1
             streaks[playerID] += 1
             correct.append(playerID)
-            # profile stats
+            # profile stats correct
             profileKeys = redis.keys(pattern='%*')
             for i in profileKeys:
                 if i == ("%" + redis.get("&" + str(playerID))):
@@ -502,7 +509,7 @@ async def on_message(message):                                                  
                 streaks[playerID] = 0
             else:
                 streaks[playerID] -= 1
-            # profile stats
+            # profile stats incorrect
             profileKeys = redis.keys(pattern='%*')
             for i in profileKeys:
                 if i == ("%" + redis.get("&" + str(playerID))):
