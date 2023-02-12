bb6da4b0ed0cdf98c1cded61472448e55cc11f08
Ethan Anderson <et714492@dal.ca>
2023-02-12 17:31:58 -0400

    rounding error

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from dreamgame.dreamhelp import dreamhelp
 
 # -- global variables --
-redis = redis.Redis.from_url(os.environ['REDIS_URL'], decode_responses=True)    # loads redis server, replace "os.environ['REDIS_URL']" with your redis URL
+redis = redis.Redis.from_url("redis://:pd461d2ca0afbb0e93ddfcda006691526b53a83f255f1420c145b5a39eec47ba9@ec2-54-145-94-157.compute-1.amazonaws.com:12649", decode_responses=True)    # loads redis server, replace "os.environ['REDIS_URL']" with your redis URL
 client = commands.Bot(command_prefix='/', intents=discord.Intents.all())        # command prefix (/)
 
 # global variables for dream journal game
@@ -474,8 +474,9 @@ async def on_message(message):                                                  
                 await message.channel.send("Your profile <@" + str(userID) + "> is assigned to " + name)
             # fetch and display stats
             stats = redis.get("%" + name).split(",")
+            # these two lines are gross and i know that
             ratio = round(int(stats[0])/(int(stats[1])+int(stats[0]))*100,1)
-            statsMsg = "**Skill Rating: " + str(round(ratio * (10 + int(stats[2])),0)) + "**"
+            statsMsg = "**Skill Rating: " + str(int(round(ratio * (10 + int(stats[2])),0))) + "**"
             statsMsg += "\nTotal Corrects: " + stats[0]
             statsMsg += "\nTotal Incorrects: " + stats[1]
             statsMsg += "\nRatio: " + str(ratio) + "%"
@@ -717,4 +718,4 @@ async def on_message(message):                                                  
             scoresPrev = scores
             scoresPrevKeys = list(scoresPrev)
 
-client.run(os.environ['BOT_TOKEN'])       #token to link code to discord bot, replace "os.environ['BOT_TOKEN']" with your token
+client.run('NjA1NTY5NDczMzY5MzQxOTcy.Xlx_0A.PrLm5bwicqFBBaT8RMkPe9L18_4')       #token to link code to discord bot, replace "os.environ['BOT_TOKEN']" with your token
